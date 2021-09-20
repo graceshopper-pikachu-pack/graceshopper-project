3efbe4f63abf836ab416396953c6cd26039ee413
Gabby Apeadu <apeadug@gmail.com>
2021-09-20 15:47:46 -0400

    created initial User routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,8 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const {
+  models: { User },
+} = require('../db');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
@@ -8,10 +10,56 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ['id', 'username'],
+    });
+    res.status(200).json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+// GET /api/user/:id (get single user)
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id); //come back and add an include
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(200).json('User not Found');
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+//POST /api/users (create a new user)
+router.post('/', async (req, res, next) => {
+  try {
+    let newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//DELETE /api/users/:id (delete user)
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await User.destroy({ where: { id: req.params.id } });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /api/user /:id (edit admin privileges)
+router.put('/:id', async (req, res, next) => {
+  try {
+    let updatedUserPrivileges = await User.findByPk(req.params.id);
+    res.json(await updatedUserPrivileges.update(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+//GET (get all of the user's orders)
