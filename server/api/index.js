4c0820ebcd8c9a093cf9ef79bc8832f9632b5eb6
Gabby Apeadu <apeadug@gmail.com>
2021-09-20 17:28:49 -0400

    fixed merge conflict

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/users', require('./users'));
+<<<<<<< HEAD
 router.use('products', require('./products'));
+=======
+>>>>>>> 6cce750c19776b1f8805dbb91e9a0169cf55e2e7
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
