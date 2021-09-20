af851dd1aa2d8e0a48d503ec125459cf418d654b
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-09-20 16:31:15 -0400

    product model added

---
+++ server/db/models/Product.js
@@ -0,0 +1,72 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+const axios = require('axios');
+
+const categories = ['pet', 'farm', 'zoo', 'wild']
+const Product = db.define('product', {
+  stockNumber: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+        notEmpty: true
+    }
+  },
+  productName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+        notEmpty: true
+    }
+  },
+  productDescription: {
+    type: Sequelize.STRING,
+    defaultValue: 'There is no description available for this item.'
+  },
+  stockQuantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+        //must be zero or greater. No negative.
+        min: 0,
+        notEmpty: true
+    }
+  },
+  imageUrl: {
+        type: Sequelize.STRING,
+        //default is a creative commons png. Change this later?
+        defaultValue: 'https://www.pinclipart.com/picdir/middle/0-3785_dj-inkers-animal-clipart-collection-zoo-clip-art.png'
+    }
+  ,
+  category: {
+    type: Sequelize.STRING,
+    validate: {
+        isIn: [categories]
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+        notEmpty: true
+    }
+
+  }
+
+})
+
+module.exports = Product
+//where does the increase and decreast of stock quantity happen?
+/**
+ * instanceMethods
+ */
+
+/**
+ * classMethods
+ */
+
+
+/**
+ * hooks
+ */
+
