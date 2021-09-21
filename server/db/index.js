e2c23e48a7fb8753b0c52d5d87caa1b5209a23fc
Miranda Stopa <miranda.stopa@gmail.com>
2021-09-21 08:28:09 -0400

    finished setting associations, removed foreignKey Id's from model definitions since they will be set by the association (please confirm)

--- server/db/index.js
+++ server/db/index.js
@@ -3,17 +3,24 @@
 const db = require("./db");
 
 const User = require("./models/User");
+const Product = require("./models/Product");
 const Cart = require("./models/Cart");
 const CartItem = require("./models/CartItem");
 const Order = require("./models/Order");
 const OrderItem = require("./models/OrderItem");
 
 //associations could go here!
-User.hasOne(Cart);
-Cart.hasOne(User);
+User.hasOne(Cart, { foreignKey: "userId" });
+Cart.belongsTo(User, { foreignKey: "userId" });
 
 User.hasMany(Order);
 Order.belongsTo(User);
+
+Product.hasOne(CartItem, { foreignKey: "productId" });
+CartItem.belongsTo(Product, { foreignKey: "productId" });
+
+Product.hasOne(OrderItem, { foreignKey: "productId" });
+OrderItem.belongsTo(Product, { foreignKey: "productId" });
 
 Cart.hasMany(CartItem);
 CartItem.belongsTo(Cart);
@@ -25,6 +32,7 @@ module.exports = {
   db,
   models: {
     User,
+    Product,
     Cart,
     CartItem,
     Order,
