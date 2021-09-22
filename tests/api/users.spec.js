a96aa13f1ce8dc782ba3a58d8ee8da914103489d
Suzanne Wong <suzannewongfl@gmail.com>
2021-09-22 15:02:55 -0400

    added Products, Users Routes to test api folder

--- tests/api/users.spec.js
+++ tests/api/users.spec.js
@@ -2,99 +2,112 @@
 
 const { expect } = require('chai');
 const request = require('supertest');
+
+const app = require('../../server/app');
+const agent = request.agent(app);
+
 const {
 	db,
 	models: { User },
 } = require('../../server/db');
-const seed = require('../../script/seed');
-const app = require('../../server/app');
+//const seed = require('../../script/seed');
 
-describe.only('User routes', () => {
-	let tina;
-	let louise;
-	let jimmy;
-
+describe('User Routes', () => {
 	beforeEach(async () => {
-		const userData = [
-			{
-				username: 'tbelcher',
-				firstName: 'Tina',
-				lastName: 'Belcher',
-				email: 'horsesRmajestic@unicorns.com',
-				adminStatus: true,
-				address: '354 Bobs Burgers Street',
-			},
-			{
-				username: 'lbelcher',
-				firstName: 'Louise',
-				lastName: 'Belcher',
-				email: 'worldDomination@muhaha.com',
-				adminStatus: false,
-				address: '354 Bobs Burgers Street',
-			},
-			{
-				username: 'jjr',
-				firstName: 'Jimmy Jr',
-				lastName: 'Pesto',
-				email: 'live2dance@stars.com',
-				adminStatus: false,
-				address: '357 Pizzeria Avenue',
-			},
-		];
-
-		const createdUsers = await Promise.all(userData.map((data) => User.create(data)));
-
-		tina = createdUsers[0];
-		louise = createdUsers[1];
-		jimmy = createdUsers[2];
+		console.log('About to sync db - will wipe out db!');
+		await db.sync({ force: true });
 	});
 
-	describe('/api/users/', () => {
-		it('GET /api/users', () => {
-			return (
-				request(app)
-					.get('/users')
-					//.expect('Content-Type', /json/)
-					.expect(200)
-					.expect((res) => {
-						expect(res.body).to.be.an.instanceOf(Array);
-						//expect(res.body.length).to.equal(5);
-					})
-			);
-		});
+	describe('User routes', () => {
+		let tina;
+		let louise;
+		let jimmy;
 
-		it('POST /api/users', () => {
-			return request(app)
-				.post('/users')
-				.send({
-					firstName: 'Gene',
+		beforeEach(async () => {
+			console.log('User route');
+			const userData = [
+				{
+					username: 'tbelcher',
+					password: 'ilovepestoboy',
+					firstName: 'Tina',
 					lastName: 'Belcher',
-					email: 'ilovemom@musiclover.com',
+					email: 'horsesRmajestic@unicorns.com',
+					adminStatus: true,
+					address: '354 Bobs Burgers Street',
+				},
+				{
+					username: 'lbelcher',
+					firstName: 'Louise',
+					lastName: 'Belcher',
+					email: 'worldDomination@muhaha.com',
 					adminStatus: false,
 					address: '354 Bobs Burgers Street',
-				})
-				.expect(201)
-				.expect('Content-Type', /json/)
-				.expect((res) => {
-					expect(res.body.firstName).to.equal('Gene');
-				});
+				},
+				{
+					username: 'jjr',
+					firstName: 'Jimmy Jr',
+					lastName: 'Pesto',
+					email: 'live2dance@stars.com',
+					adminStatus: false,
+					address: '357 Pizzeria Avenue',
+				},
+			];
+			const createdUsers = await Promise.all(userData.map((data) => User.create(data)));
+
+			tina = createdUsers[0];
+			louise = createdUsers[1];
+			jimmy = createdUsers[2];
 		});
 
-		it('DELETE /api/users/:id', async () => {
-			await request(app).delete(`/users/${louise.id}`).expect(204);
-			const deletedUser = await User.findByPk(louise.id);
-			expect(deletedUser).to.equal.null;
-		});
+		describe('/api/users/', () => {
+			it('GET /api/users', () => {
+				return (
+					request(app)
+						.get('/users')
+						//.expect('Content-Type', /json/)
+						.expect(200)
+						.expect((res) => {
+							expect(res.body).to.be.an('array');
+							//expect(res.body.length).to.equal(5);
+						})
+				);
+			});
 
-		it('PUT /api/users/:id', () => {
-			return request(app)
-				.put(`/users/${tina.id}`)
-				.send({ firstName: 'Linda' })
-				.expect(200)
-				.expect('Content-Type', /json/)
-				.expect((res) => {
-					expect(res.body.firstName).to.equal('Linda');
-				});
-		}); // end describe('/api/users')
-	}); // end describe('User routes')
+			it('POST /api/users', () => {
+				return request(app)
+					.post('/users')
+					.send({
+						firstName: 'Gene',
+						lastName: 'Belcher',
+						email: 'ilovemom@musiclover.com',
+						adminStatus: false,
+						address: '354 Bobs Burgers Street',
+					})
+					.expect(201)
+					.expect('Content-Type', /json/)
+					.expect((res) => {
+						expect(res.body.firstName).to.equal('Gene');
+					});
+			});
+
+			it('DELETE /api/users/:id', async () => {
+				await request(app).delete(`/users/${louise.id}`).expect(204);
+				const deletedUser = await User.findByPk(louise.id);
+				expect(deletedUser).to.equal.null;
+			});
+
+			it('PUT /api/users/:id', () => {
+				return (
+					request(app)
+						.put(`/users/${tina.id}`)
+						.send({ firstName: 'Linda' })
+						.expect(200)
+						//.expect('Content-Type', /json/)
+						.expect((res) => {
+							expect(res.body.firstName).to.equal('Linda');
+						})
+				);
+			}); // end describe('/api/users')
+		}); // end describe('User routes')
+	});
 });
