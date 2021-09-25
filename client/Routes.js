45dd82ebaf774176841a424f5cd5f57bace0c8a0
Suzanne Wong <suzannewongfl@gmail.com>
2021-09-25 14:22:23 -0400

    edited Routes for Home logic, commented out isAdmin logic

--- client/Routes.js
+++ client/Routes.js
@@ -7,64 +7,67 @@ import Home from './components/Home';
 import ProductsList from './components/ProductsList';
 import SingleProduct from './components/SingleProduct';
 import Cart from './components/Cart';
-import Admin from './components/AdminPage';
+
 import { me } from './store';
 import AdminPage from './components/AdminPage';
+//import AdminDashboard from './components/AdminDashboard';
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+	componentDidMount() {
+		this.props.loadInitialData();
+	}
 
-  render() {
-    const { isLoggedIn } = this.props;
-
-    return (
-      <div>
-        {/* code for GENERAL VIEWERS */}
-        <Switch>
-          <Route path="/home" component={ProductsList} />
-          <Route exact path="/products" component={ProductsList} />
-          <Route exact path="/products/:productId" component={SingleProduct} />
-          <Route exact path="/cart" component={Cart} />
-          {!isLoggedIn && (
-            <>
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-              <Route exact path="/admin" component={AdminPage} />
-            </>
-            /* {!isAdmin && (
-					<Switch>
-						<Route exact path="/admin" exact component={AdminPage} />
-					</Switch> */
-          )}
-          <Redirect to="/home" />
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		const { isLoggedIn } = this.props;
+		return (
+			<div>
+				{/* code for GENERAL VIEWERS */}
+				<Switch>
+					<Route exact path="/" component={ProductsList} />
+					<Route exact path="/products" component={ProductsList} />
+					<Route exact path="/products/:productId" component={SingleProduct} />
+					<Route exact path="/cart" component={Cart} />
+					{!isLoggedIn ? (
+						<>
+							<Route path="/login" component={Login} />
+							<Route path="/signup" component={Signup} />
+							<Route exact path="/home" component={ProductsList} />
+						</>
+					) : (
+						<Route exact path="/home" component={Home} />
+					)}
+					{/* {isAdmin && (
+						<>
+							<Route exact path="/admin" component={AdminPage} />
+						</>
+					)} */}
+					<Redirect to="/home" />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
+	return {
+		// Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+		// Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+		isLoggedIn: !!state.auth.id,
+	};
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
+	return {
+		loadInitialData() {
+			dispatch(me());
+		},
+	};
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
