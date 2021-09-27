7c5fd8aade4db199a029aec57f83f772172a9b05
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-09-27 17:28:39 -0400

    delete commented out code

--- client/components/AdminViewUsers.js
+++ client/components/AdminViewUsers.js
@@ -29,25 +29,7 @@ class AdminView extends React.Component{
 
     return (
       <table>
-        {/* {this.props.users.map((adminUser) => {
-          <div>
-          <Checkbox
-          id={adminUser.id}
-          name={adminUser.username}
-          value={adminUser.username}
-          checked={adminUser.checked}
-          onChange={() => this.props.handleChecked(props.index, adminUser.id)}
-        />
-        <div className="admin-dashboard-username">{adminUser.username}</div>
-        <div className="admin-dashboard-firstName">{adminUser.firstName}</div>
-        <div className="admin-dashboard-lastname">{adminUser.lastName}</div>
-        <div className="admin-dashboard-email">{adminUser.email}</div>
-        <div className="admin-dashboard-address">{adminUser.address}</div>
-        <div className="admin-dashboard-adminstatus">{adminUser.adminStatus}</div>
-        <Link to={route}>Edit</Link>
-        </div>
-        })
-      } */}
+
       {adminUsers.map((adminUser) => {
         return(
           <tbody key={adminUser.id}>
