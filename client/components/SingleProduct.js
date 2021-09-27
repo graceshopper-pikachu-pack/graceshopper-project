ce6d8d2d399859caf2c910fecb81185e3cd29dd8
Suzanne Wong <suzannewongfl@gmail.com>
2021-09-27 16:03:22 -0400

    merged SingleProduct from main into tests1

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,14 +1,8 @@
-import React from "react";
-import { connect } from "react-redux";
-import {
-  fetchSingleProduct,
-  clearSingleProduct,
-  editCart,
-  fetchCartItem,
-} from "../store";
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchSingleProduct, clearSingleProduct, editCart, fetchCartItem } from '../store';
 
 class SingleProduct extends React.Component {
-<<<<<<< HEAD
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -135,143 +129,22 @@ class SingleProduct extends React.Component {
 			</div>
 		);
 	}
-=======
-  constructor(props) {
-    super(props);
-    this.state = {
-      singleProduct: {},
-      quantity: 0,
-      cartItemId: "",
-      errors: {
-        quantity: "",
-      },
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.fetchSingleProduct(this.props.match.params.productId);
-
-    // handles in the redux store for both guest and signed in
-    this.props.fetchCartItem({
-      productId: this.props.match.params.productId,
-    });
-  }
-
-  componentWillUnmount() {
-    this.props.clearSingleProduct();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.singleProduct !== this.props.singleProduct) {
-      this.setState({
-        singleProduct: this.props.singleProduct,
-      });
-    }
-    if (prevProps.singleCartItem !== this.props.singleCartItem) {
-      this.setState({
-        quantity: this.props.singleCartItem.quantity,
-        cartItemId: this.props.singleCartItem.id,
-      });
-    }
-  }
-
-  handleChange(evt) {
-    // front-end error handling
-    const { name, value } = evt.target;
-    // make a copy of the state's errors
-    let errors = this.state.errors;
-    // error handling for valid quantity
-    if (isNaN(value) || parseInt(value) !== Number(value) || value < 0) {
-      errors.quantity = "Please input a valid number";
-    } else if (value > this.state.singleProduct.stockQuantity) {
-      errors.quantity = "Requested quantity in cart exceeds stock quantity";
-    } else {
-      errors.quantity = "";
-    }
-
-    // sets the copy of the error state we made with the changes we implemented through switch on the state
-
-    this.setState({
-      errors,
-      [name]: value,
-    });
-  }
-
-  handleEdit(evt) {
-    evt.preventDefault();
-
-    // if there are no errors in the quantity
-    if (!this.state.errors.quantity) {
-      this.props.editCart({
-        productId: this.state.singleProduct.id,
-        cartItemId: this.state.cartItemId,
-        quantity: this.state.quantity,
-      });
-    }
-  }
-
-  render() {
-    const singleProduct = this.state.singleProduct || {};
-    const { quantity, errors } = this.state;
-
-    return (
-      <div>
-        {!singleProduct.id ? (
-          <h4>Loading...</h4>
-        ) : (
-          <div id="single-product" className="column">
-            <div id="single-product-detail" className="row">
-              <img src={singleProduct.imageUrl} />
-              <div className="mr1">
-                <h2>Product Name: {singleProduct.productName}</h2>
-                <h3>Price: {singleProduct.price}</h3>
-                <h3>Product Description: {singleProduct.productDescription}</h3>
-
-                {errors.quantity ? (
-                  <h6 className="error">{errors.quantity}</h6>
-                ) : null}
-                <label htmlFor="quantity">
-                  <small>Current Quantity:</small>
-                </label>
-                <input
-                  name="quantity"
-                  type="text"
-                  value={quantity}
-                  onChange={this.handleChange}
-                  style={{
-                    border: errors.quantity
-                      ? "2px solid red"
-                      : this.state.value,
-                  }}
-                />
-
-                <button onClick={this.handleEdit}>Add to Cart</button>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
->>>>>>> 01a86a5bdaf9115ae62f4d37bb858ae8a2a5479d
 }
 
 const mapState = (state) => {
-  return {
-    singleProduct: state.singleProduct,
-    singleCartItem: state.singleCartItem,
-  };
+	return {
+		singleProduct: state.singleProduct,
+		singleCartItem: state.singleCartItem,
+	};
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    fetchSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
-    clearSingleProduct: () => dispatch(clearSingleProduct()),
-    editCart: (product) => dispatch(editCart(product)),
-    fetchCartItem: (productId) => dispatch(fetchCartItem(productId)),
-  };
+	return {
+		fetchSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
+		clearSingleProduct: () => dispatch(clearSingleProduct()),
+		editCart: (product) => dispatch(editCart(product)),
+		fetchCartItem: (productId) => dispatch(fetchCartItem(productId)),
+	};
 };
 
 export default connect(mapState, mapDispatch)(SingleProduct);
