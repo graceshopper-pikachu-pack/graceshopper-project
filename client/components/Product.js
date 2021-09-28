f45a9879a52f1ee7cbdbc2a8b001fe36bdee4380
Miranda Stopa <miranda.stopa@gmail.com>
2021-09-28 11:04:34 -0400

    fixed filter

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,41 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Product = (props) => {
-	const routeToProduct = () => {
-		const route = `/products/${props.product.id}`;
-		props.history.push(route);
-	};
+  const routeToProduct = () => {
+    const route = `/products/${props.product.id}`;
+    props.history.push(route);
+  };
 
-	const singleProduct = props.product;
+  const singleProduct = props.product;
 
-	return (
-		<div>
-			<article>
-				<img src={singleProduct.imageUrl} onClick={routeToProduct} class="img-products-list" />
-				<div class="text">
-					<h3>{singleProduct.productName.toUpperCase()}</h3>
-					<h4>${singleProduct.price.toLocaleString()}</h4>
-					<h4>Quantity: {singleProduct.quantity}</h4>
-					{singleProduct.errors ? <h6 className="error">{singleProduct.errors}</h6> : null}
-					<button
-						onClick={(evt) => {
-							props.handleDecrement(singleProduct, evt);
-						}}
-					>
-						Subtract from Cart
-					</button>
+  return (
+    <div>
+      <article>
+        <img
+          src={singleProduct.imageUrl}
+          onClick={routeToProduct}
+          className="img-products-list"
+        />
+        <div className="text">
+          <h3>{singleProduct.productName.toUpperCase()}</h3>
+          <h4>${singleProduct.price.toLocaleString()}</h4>
+          <h4>Quantity: {singleProduct.quantity}</h4>
+          {singleProduct.errors ? (
+            <h6 className="error">{singleProduct.errors}</h6>
+          ) : null}
+          <button
+            onClick={(evt) => {
+              props.handleDecrement(singleProduct, evt);
+            }}
+          >
+            Subtract from Cart
+          </button>
 
-					<button
-						onClick={(evt) => {
-							props.handleIncrement(singleProduct, evt);
-						}}
-					>
-						Add to Cart
-					</button>
-				</div>
-			</article>
-		</div>
-	);
+          <button
+            onClick={(evt) => {
+              props.handleIncrement(singleProduct, evt);
+            }}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default Product;
